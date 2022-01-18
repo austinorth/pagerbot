4a268c03f3265a2bec5fb65054382dd88dea8c73
Austin Orth <austin.orth@niche.com>
2022-01-18 09:56:09 -0500

    Formatting changes, added new Dockerfile
    Created a new Dockerfile that follows best practices.
    Ran all files through go fmt to format them correctly.
    Removed a couple more extranesous files from previous use.
    
    Signed-off-by: Austin Orth <austin.orth@niche.com>

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,39 @@
-FROM goodeggs/platform-base:2.6.0
-COPY --chown=docker:docker dist/linux_amd64/pagerbot config.yml ./
+FROM docker.io/library/golang:1.17 AS builder
+
+# Create the user and group files that will be used in the running container to
+# run the process as an unprivileged user.
+RUN mkdir /user && \
+    echo 'nobody:x:65534:65534:nobody:/:' > /user/passwd && \
+    echo 'nobody:x:65534:' > /user/group
+
+# Set the working directory outside $GOPATH to enable the support for modules.
+WORKDIR /src
+
+# Warm the build cache with a non-cgo (i.e. static) standard library build
+# (a go.mod file must exist or it will not run in module-aware mode, and will
+# therefore not warm the cache)
+RUN go mod init temp 2>&1
+RUN CGO_ENABLED=0 go build -installsuffix 'static' std
+
+# Import the code from the context.
+COPY ./ ./
+
+# Build the executable to `/pagerbot`. Mark the build as statically linked.
+RUN CGO_ENABLED=0 go build \
+    -installsuffix 'static' \
+    -o /pagerbot .
+
+# Final stage: the running container.
+FROM scratch AS final
+
+# Import the user and group files from the first stage.
+COPY --from=builder /user/group /user/passwd /etc/
+
+# Import the compiled executable from the second stage.
+COPY --from=builder /pagerbot /pagerbot
+
+# Perform any further action as an unprivileged user.
+USER nobody:nobody
+
+# Run the compiled binary.
+ENTRYPOINT ["/pagerbot"]
--- updater/updater.go
+++ updater/updater.go
@@ -1,39 +1,40 @@
 package updater
 
-import(
-  "time"
-  "sync"
-  "github.com/yosmudge/pagerbot/config"
-  "github.com/yosmudge/pagerbot/slack"
-  "github.com/yosmudge/pagerbot/pagerduty"
+import (
+	"sync"
+	"time"
+
+	"github.com/yosmudge/pagerbot/config"
+	"github.com/yosmudge/pagerbot/pagerduty"
+	"github.com/yosmudge/pagerbot/slack"
 )
 
-type Updater struct{
-  Wg          *sync.WaitGroup
-  Slack       *slack.Api
-  Pagerduty   *pagerduty.Api
-  Users       *UserList
-  Schedules   *ScheduleList
-  LastFetch   time.Time
+type Updater struct {
+	Wg        *sync.WaitGroup
+	Slack     *slack.Api
+	Pagerduty *pagerduty.Api
+	Users     *UserList
+	Schedules *ScheduleList
+	LastFetch time.Time
 }
 
-func New() (*Updater, error){
-  u := Updater{}
-  u.Wg = &sync.WaitGroup{}
+func New() (*Updater, error) {
+	u := Updater{}
+	u.Wg = &sync.WaitGroup{}
 
-  var err error
-  u.Slack, err = slack.New(config.Config.ApiKeys.Slack)
-  if err != nil {
-    return &u, err
-  }
+	var err error
+	u.Slack, err = slack.New(config.Config.ApiKeys.Slack)
+	if err != nil {
+		return &u, err
+	}
 
-  u.Pagerduty, err = pagerduty.New(config.Config.ApiKeys.Pagerduty.Key, config.Config.ApiKeys.Pagerduty.Org)
-  if err != nil {
-    return &u, err
-  }
+	u.Pagerduty, err = pagerduty.New(config.Config.ApiKeys.Pagerduty.Key, config.Config.ApiKeys.Pagerduty.Org)
+	if err != nil {
+		return &u, err
+	}
 
-  u.Users = &UserList{}
-  u.Schedules = &ScheduleList{}
+	u.Users = &UserList{}
+	u.Schedules = &ScheduleList{}
 
-  return &u, nil
+	return &u, nil
 }
